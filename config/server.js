fad22b735b42120f4606a61339d81280568db797
Jorge Arturo Arellano del Aguila <jorch1910@gmail.com>
2021-01-11 19:53:54 -0700

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '58ea606a93189d71cc78452b9a5d6494'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f6fcc9fa-0ba6-46f9-a7b4-e7d0a8885139'
+};
